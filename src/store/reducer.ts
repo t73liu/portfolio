08da4c8a39ef2ea82afa35f1490819328b4f7cc6
t73liu <t73liu@gmail.com>
2018-04-17 09:15:42 -0400

    add redux to package.json

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,11 +1,12 @@
 import {AsyncStorage} from 'react-native';
 import hardSet from 'redux-persist/es/stateReconciler/hardSet';
+import {PersistConfig} from "redux-persist/es/types";
 import {persistCombineReducers} from 'redux-persist/es/persistCombineReducers';
 
 import {WatchlistReducer} from './watchlist/reducer';
 import {StoreState} from './types';
 
-const persistConfig = {
+const persistConfig: PersistConfig = {
     key: 'root',
     storage: AsyncStorage,
     stateReconciler: hardSet,
