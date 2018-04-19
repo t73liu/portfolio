2b71be403958c4bc00ea6e920e5e7d89cdbbd476
t73liu <t73liu@gmail.com>
2018-04-18 22:55:04 -0400

    Connect store to StockDetail component

--- src/watchlist/StockDetail.tsx
+++ src/watchlist/StockDetail.tsx
@@ -3,10 +3,13 @@ import {Body, Button, Icon, ListItem, Right, Text} from 'native-base';
 import {connect} from 'react-redux';
 import {Dispatch} from 'redux';
 
-import {StoreState} from "../store/types";
+import {Quote, StoreState} from "../store/types";
+import {removeWatchlistTicker} from "../store/watchlist/actions";
 
 interface StockDetailProps {
     ticker: string
+    quote: Quote
+    dispatch: Dispatch<StoreState>
 }
 
 interface StockDetailState {
@@ -18,17 +21,36 @@ class StockDetail extends Component<StockDetailProps, StockDetailState> {
 
     constructor(props: StockDetailProps) {
         super(props);
+        this.changeView = this.changeView.bind(this);
+        this.deleteTicker = this.deleteTicker.bind(this);
+    }
+
+    changeView() {
+        this.setState({
+            pressed: !this.state.pressed
+        });
+    }
+
+    deleteTicker() {
+        this.props.dispatch(removeWatchlistTicker(this.props.ticker));
     }
 
     render() {
         return (
-            <ListItem>
-                <Body>
-                <Text>{this.props.ticker}</Text>
-                <Text note>Placeholder</Text>
-                </Body>
+            <ListItem onPress={this.changeView}>
+                {this.state.pressed ?
+                    (<Body>
+                    <Text>
+                        Test
+                    </Text>
+                    </Body>) :
+                    (<Body>
+                    <Text>{this.props.ticker}</Text>
+                    <Text note>{this.props.quote.ytdChange}</Text>
+                    </Body>)
+                }
                 <Right>
-                    <Button danger transparent>
+                    <Button dark transparent onPress={this.deleteTicker}>
                         <Icon name="trash"/>
                     </Button>
                 </Right>
@@ -38,11 +60,17 @@ class StockDetail extends Component<StockDetailProps, StockDetailState> {
 }
 
 function mapStateToProps(state: StoreState, ownProps: StockDetailProps): StockDetailProps {
-    return ownProps;
+    return {
+        quote: state.marketData.symbolData.get(ownProps.ticker)!.quote,
+        ...ownProps
+    };
 }
 
 function mapDispatchToProps(dispatch: Dispatch<StoreState>, ownProps: StockDetailProps): StockDetailProps {
-    return ownProps;
+    return {
+        dispatch,
+        ...ownProps
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(StockDetail);
