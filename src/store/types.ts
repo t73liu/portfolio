0de5fa84f2cd29a2b4cbeaec12ba2815141bed85
t73liu <t73liu@gmail.com>
2018-04-18 22:54:42 -0400

    Update store interface

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,29 +1,29 @@
 import NewsItem from '../news/NewsItem';
 
 export interface Quote {
-    symbol: string,
-    companyName: string,
-    primaryExchange: string,
-    sector: string,
-    open: number,
-    openTime: number, // millisecond timestamp to be formatted
-    close: number,
-    closeTime: number, // millisecond timestamp to be formatted
-    high: number,
-    low: number,
-    latestPrice: number,
-    latestUpdate: number, // millisecond timestamp to be formatted
-    latestVolume: number,
-    delayedPrice: number,
-    delayedPriceTime: number, // millisecond timestamp to be formatted
-    previousClose: number,
-    change: number,
-    changePercent: number,
-    avgTotalVolume: number,
-    marketCap: number,
-    peRatio: number,
-    week52High: number,
-    week52Low: number,
+    // symbol: string,
+    // companyName: string,
+    // primaryExchange: string,
+    // sector: string,
+    // open: number,
+    // openTime: number, // millisecond timestamp to be formatted
+    // close: number,
+    // closeTime: number, // millisecond timestamp to be formatted
+    // high: number,
+    // low: number,
+    // latestPrice: number,
+    // latestUpdate: number, // millisecond timestamp to be formatted
+    // latestVolume: number,
+    // delayedPrice: number,
+    // delayedPriceTime: number, // millisecond timestamp to be formatted
+    // previousClose: number,
+    // change: number,
+    // changePercent: number,
+    // avgTotalVolume: number,
+    // marketCap: number,
+    // peRatio: number,
+    // week52High: number,
+    // week52Low: number,
     ytdChange: number
 }
 
@@ -57,14 +57,20 @@ export interface MultiDayChart {
     vwap: number
 }
 
-export interface MarketData {
+export interface SymbolData {
     quote: Quote
     news: NewsItem[]
 }
 
+export interface MarketData {
+    symbolData: Map<string, SymbolData>
+    isRefreshing: boolean
+    lastUpdated: Date
+}
+
 // TODO implement proper interface for market data and portfolio
 export interface StoreState {
-    marketData: Map<string, MarketData>
+    marketData: MarketData
     portfolio: object[]
     watchlist: string[]
 }
